9aac595a7a26e287f30a5fc202dc92ab25039c50
SHANDY-ARTHAMA-PUTRA <164329890+SHANDY-ARTHAMA-PUTRA@users.noreply.github.com>
2024-04-29 12:35:00 +0800

    Penerapan if else & switch case di java

---
+++ Ifelse.java
@@ -0,0 +1,10 @@
+public class Ifelse {
+  public static void main(String[] args) {
+    String warna_lampu = "Merah";
+    
+    if (warna_lampu == "Merah") {
+        System.out.println("Indikator air panas menyala");
+    }else{
+        System.out.println("Indikator tidak terdeteksi");
+  }
+}}
---
+++ Switchcase.java
@@ -0,0 +1,17 @@
+public class Switchcase {
+  public static void main(String[] args) {
+    int menu=2;
+
+    switch (menu){
+        case 1:
+            System.out.println("Air Mineral");
+            break;
+        case 2:
+            System.out.println("Air Reverse Osmosis");
+            break;
+        default:
+            System.out.println("Pilihan tidak tersedia");
+      }
+   }
+}
+    
